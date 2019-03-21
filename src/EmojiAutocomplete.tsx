a69cbfffdc59bfb1f4bf28e5d1578bb1516fd1f7
Larry512 <jaredpotter1@gmail.com>
2019-03-21 00:23:50 -0600

    Initial React / TypeScript Emoji Autocomplete component.

---
+++ src/EmojiAutocomplete.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export interface EmojiAutocompleteProps { }
+
+export default class EmojiAutocomplete extends React.Component<EmojiAutocompleteProps, {}> {
+    render() {
+        return <h1>Hello, World!</h1>;
+    }
+}
