9eb430ea060e00fe2f72975804774beed667e19c
Larry512 <jaredpotter1@gmail.com>
2019-03-21 00:35:59 -0600

    Starting to stub out component.

--- src/EmojiAutocomplete.tsx
+++ src/EmojiAutocomplete.tsx
@@ -1,11 +1,42 @@
 import * as React from "react";
 
-export interface EmojiAutocompleteProps { }
+export interface EmojiAutocompleteProps { 
 
-export default class EmojiAutocomplete extends React.Component<EmojiAutocompleteProps, {}> {
+}
+export interface EmojiAutocompleteState { 
+    currentText: string;
+}
+
+export default class EmojiAutocomplete extends React.Component<EmojiAutocompleteProps, EmojiAutocompleteState> {
+    constructor(props : any) {
+        super(props);
+        
+        this.state = {
+            currentText: ''
+        };
+    }
+
+    handleChange = (e : any) => {
+        const value = e.target.value;
+        debugger;
+        // console.log(value);
+    
+        // this.state.onSelectChange(value);
+      }    
+
     render() {
         return (
-            <h1>Hello, World!</h1>
+            <div>
+                {/* <textarea class="conversation-input-text border-theme" value={this.state.currentText} onChange={this.handleChange}>
+                </textarea>                 */}
+                <textarea 
+                    value={this.state.currentText}
+                    onChange={this.handleChange}
+                    className="conversation-input-text border-theme"
+                >
+
+                </textarea>
+            </div>
         );
     };
 }
